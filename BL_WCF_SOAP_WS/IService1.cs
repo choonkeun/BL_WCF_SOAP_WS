de2b207c288d315592b8439dceec94ce4e1e9ba7
choonkeun <choonkeun@yahoo.com>
2017-07-17 09:08:29 -0700

    Build

---
+++ BL_WCF_SOAP_WS/IService1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace BL_WCF_SOAP_WS
+{
+    [ServiceContract]
+    [XmlSerializerFormat]
+    public interface IService1
+    {
+        [OperationContract]
+        List<ToDoItem> GetToDos();
+
+        [OperationContract]
+        void AddOrUpdateToDo(ToDoItem toDoItem);
+
+        [OperationContract]
+        void DeleteToDo(ToDoItem toDoItem);
+    }
+
+    [DataContract]
+    public class ToDoItem
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
